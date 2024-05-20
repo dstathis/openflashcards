40b1d91f8d3702863ad75d763490237e301d8181
Dylan Stephano-Shachter <dylan@theone.ninja>
2024-05-20 02:22:15 +0200

    web app

--- ofc.go
+++ ofc.go
@@ -1,14 +1,73 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
 	"math/rand"
-	"os"
-
-	"github.com/tsuru/gnuflag"
+	"net/http"
+	"strconv"
+	"strings"
 )
+
+var sessions map[int]*Session = make(map[int]*Session)
+var nextSessionId int = 0
+
+type Session struct {
+	deck []Card
+	curCard int
+	curSide int
+}
+
+func getDeckJson(addr string) ([]byte, error) {
+	response, err := http.Get(addr)
+	if err != nil {
+		fmt.Println("get_error")
+		return []byte(""), err
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("read_error")
+		return []byte(""), err
+	}
+	return body, nil
+}
+
+func (session *Session) loadDeck(deckAddress string) error {
+	deckString, err := getDeckJson(deckAddress)
+	if err != nil {
+		return err
+	}
+	var orderedDeck []Card
+	err = json.Unmarshal(deckString, &orderedDeck)
+	if err != nil {
+		return err
+	}
+	deckLength := len(orderedDeck)
+	session.deck = make([]Card, deckLength, deckLength)
+	order := rand.Perm(deckLength)
+	for i, j := range order {
+		session.deck[i] = orderedDeck[j]
+	}
+	return nil
+}
+
+func (session *Session) next() string {
+	if session.curCard == len(session.deck) {
+		return "Done!"
+	}
+	if session.curSide == 0 {
+		text := session.deck[session.curCard].Front
+		session.curSide++
+		return text
+	}
+	text := session.deck[session.curCard].Back
+	session.curSide = 0
+	session.curCard++
+	return text
+}
 
 type Card struct {
 	Front   string `json:"front"`
@@ -16,51 +75,38 @@ type Card struct {
 	Learned bool   `json:"learned"`
 }
 
+type Page struct {
+	CardText string
+	Link string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	firstPath := strings.Split(r.URL.Path, "/")[1]
+	sessionId, err := strconv.Atoi(firstPath)
+
+	// We need to create a new session ID
+	if err != nil {
+		sessionId = nextSessionId
+		nextSessionId++
+		sessions[sessionId] = &Session{[]Card{}, 0, 0}
+		err := (*sessions[sessionId]).loadDeck("http://" + r.URL.Path[1:])
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	session := sessions[sessionId]
+	link := fmt.Sprintf("/%d", sessionId)
+	page := &Page{CardText: session.next(), Link: link}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, page)
+}
+
+func nullHandler(w http.ResponseWriter, r *http.Request) {}
+
 func main() {
 
-	var reverse bool
-	gnuflag.BoolVar(&reverse, "reverse", false, "Allow cards to be presented in reverse.")
-	gnuflag.Parse(true)
-	var args []string
-	if gnuflag.NArg() == 0 {
-		args = []string{"example.json"}
-	} else {
-		args = gnuflag.Args()
-	}
-
-	var deck []Card
-	deckString, err := os.ReadFile(args[0])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = json.Unmarshal(deckString, &deck)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	order := rand.Perm(len(deck))
-	for _, i := range order {
-		card := deck[i]
-		var a string
-		var b string
-		if reverse {
-			if rand.Intn(2) == 0 {
-				a = card.Front
-				b = card.Back
-			} else {
-				a = card.Back
-				b = card.Front
-			}
-		} else {
-			a = card.Front
-			b = card.Back
-		}
-		fmt.Print(a)
-		fmt.Print("\n")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-		fmt.Print(b)
-		fmt.Print("\n")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-	}
+	http.HandleFunc("/favicon.ico", nullHandler)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
